e3ad5ac5cc0ee771eb7e4209f7e5f995a7fbe536
Janeil Alcano <janeilalcano@yahoo.com.ph>
2019-04-11 15:39:24 +0800

    Video 16

---
+++ src/app/employee/create-employee.component.ts
@@ -0,0 +1,148 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { AbstractEmitterVisitor } from '@angular/compiler/src/output/abstract_emitter';
+import { validateConfig } from '@angular/router/src/config';
+import { elementStyleProp } from '@angular/core/src/render3';
+//import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+  employeeForm: FormGroup;
+
+  validationMessages = {
+    'fullName': {
+      'required': 'Full Name is required.',
+      'minlength': 'Full Name must be greater than 2 characters.',
+      'maxlength': 'Full Name must be less than 10 characters.'
+    },
+    'email': {
+      'required': 'Email is required.',
+      'emailDomain': 'Email domain should be domain.com.'
+    },
+    'phone': {
+      'required': 'Phone is required.'
+    },
+    'skillName': {
+      'required': 'Skill Name is required.'
+    },
+    'experienceInYears': {
+      'required': 'Experience is required.'
+    },
+    'proficiency': {
+      'required': 'Proficiency is required.'
+    }
+
+  };
+
+  formErrors = {
+    'fullName': '',
+    'email': '',
+    'phone': '',
+    'skillName': '',
+    'experienceInYears': '',
+    'proficiency': ''
+  };
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.employeeForm = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      contactPreference: [''],
+      email: [''],
+      phone: [''],
+      skills: this.fb.group({
+        skillName: ['', Validators.required],
+        experienceInYears: ['', Validators.required],
+        proficiency: ['', Validators.required]
+      })
+    });
+
+    this.employeeForm.get('contactPreference').valueChanges.subscribe((data: string) => {
+      this.onContactPreferenceChange(data);
+    });
+    // When any of the form control value in employee form changes
+    // our validation function logValidationErrors() is called
+    this.employeeForm.valueChanges.subscribe((data) => {
+      this.logValidationErrors(this.employeeForm);
+    });
+  }
+
+  //Contact Preference
+  onContactPreferenceChange(selectedValue: string) {
+    const emailFormControl = this.employeeForm.get('email');
+    const phoneFormControl = this.employeeForm.get('phone');
+
+    if (selectedValue == 'email') {
+      emailFormControl.setValidators([Validators.required, emailDomain('domain.com')]);
+    } else {
+      emailFormControl.clearValidators();
+    }
+    emailFormControl.updateValueAndValidity();
+
+    if (selectedValue == 'phone') {
+      phoneFormControl.setValidators(Validators.required);
+    } else {
+      phoneFormControl.clearValidators();
+    }
+    phoneFormControl.updateValueAndValidity();
+  }
+
+  logValidationErrors(group: FormGroup = this.employeeForm): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractControl = group.get(key);
+      if (abstractControl instanceof FormGroup) {
+        //abstractControl.disable();
+
+        //Recursively call the method to loop through nested formgroups
+        this.logValidationErrors(abstractControl);
+      } else {
+
+        this.formErrors[key] = '';
+        if (abstractControl && !abstractControl.valid && (abstractControl.touched || abstractControl.dirty)) {
+          const messages = this.validationMessages[key];
+
+          for (const errorKey in abstractControl.errors) {
+            if (errorKey) {
+              this.formErrors[key] += messages[errorKey] + ' ';
+            }
+          }
+        }
+
+      }
+    });
+  }
+
+
+  onLoadDataClick(): void {
+
+  }
+
+
+
+  onSubmit(): void {
+    console.log(this.employeeForm.touched);
+    console.log(this.employeeForm.value);
+
+    console.log(this.employeeForm.controls.fullName.touched);
+    console.log(this.employeeForm.get('fullName').value);
+  }
+
+}
+
+function emailDomain(domainName: string) {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const email: string = control.value;
+    const domain = email.substring(email.lastIndexOf('@') + 1);
+    if (email === '' || domain.toLowerCase() === domainName.toLowerCase()) {
+      return null;
+    } else {
+      return { 'emailDomain': true };
+    }
+  };
+}
