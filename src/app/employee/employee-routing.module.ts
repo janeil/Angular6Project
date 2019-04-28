1a24dcbd11bd360c9b2b2099189cd958f36fe718
Janeil Alcano <janeilalcano@yahoo.com.ph>
2019-04-29 01:17:11 +0800

    Video 36: Lazy Loading

--- src/app/employee/employee-routing.module.ts
+++ src/app/employee/employee-routing.module.ts
@@ -4,13 +4,9 @@ import { ListEmployeesComponent } from './list-employees.component';
 import { CreateEmployeeComponent } from './create-employee.component';
 
 const appRoutes: Routes = [
-    {
-        path: 'employees', children: [
-            { path: '', component: ListEmployeesComponent },
-            { path: 'create', component: CreateEmployeeComponent },
-            { path: 'edit/:id', component: CreateEmployeeComponent },
-        ]
-    }
+    { path: '', component: ListEmployeesComponent },
+    { path: 'create', component: CreateEmployeeComponent },
+    { path: 'edit/:id', component: CreateEmployeeComponent },
 
 ];
 
